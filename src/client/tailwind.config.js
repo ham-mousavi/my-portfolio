6cd3577032ffb12c4b4572ca224dd6bebda525d0
mrv90 <hamid.mousavinasab@gamil.com>
2024-06-22 13:44:57 +0330

    adding custome color using tailwind

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'MyGray': '808080',
+        'MyFeijoa': 'A4DC7C',
+        'MyOrange': 'E07D54',
+        'MyYellowGeranium': 'FFE1A0',
+        'MySkyBlue': '8BC3E4'
+      }
+    },
   },
   plugins: [],
 }
